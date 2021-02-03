b2ce9810a0f23fdc96ac8893215358ef3bd9f0a7
Kutay Yaman <yamankutay1@gmail.com>
2021-02-03 21:22:56 +0300

    Fontawesome added and signuppage is done.

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { signup } from '../api/apiCalls';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelopeSquare, faLock, faUser } from '@fortawesome/free-solid-svg-icons'
 
 class UserSignupPage extends React.Component {
 
@@ -50,39 +52,58 @@ class UserSignupPage extends React.Component {
         const { pendingApiCall } = this.state;
         return (
             <div className="container">
-                <form>
-                    <h2 className="text-center">Sign Up</h2>
-                    <div className="form-group">
-                        <label>Your name</label>
-                        <input className="form-control" name="name" onChange={this.onChange} />
-                    </div>
-                    <div className="form-group">
-                        <label>Surname</label>
-                        <input className="form-control" name="surname" onChange={this.onChange} />
-                    </div>
-                    <div className="form-group">
-                        <label>Email</label>
-                        <input className="form-control" name="email" onChange={this.onChange} />
-                    </div>
-                    <div className="form-group">
-                        <label>Password</label>
-                        <input className="form-control" name="password" type="password" onChange={this.onChange} />
-                    </div>
-                    <div className="form-group">
-                        <label>Re-enter password</label>
-                        <input className="form-control" name="reEnterPassword" type="password" onChange={this.onChange} />
-                    </div>
-                    <div className="text-center">
-                        <button disabled={pendingApiCall} className="btn btn-primary" onClick={this.onClickSignup}>Create Your Account</button>
-                    </div>
-                    <div className="text-center mt-2">
-                        {pendingApiCall && //conditional rendering deniyor buna pendingApiCall dogruysa devamindaki calisir
-                            <div class="spinner-border" role="status">
-                                <span class="sr-only">Loading...</span>
-                            </div>}
-                    </div>
+                <div className="card mt-2">
+                    <div className="card-body">
+                        <form>
+                            <h2 className="text-center">Add User</h2>
+                            <div className="form-group">
+                                <label>Your name</label>
+                                <div className="input-group-append">
+                                    <span className="input-group-text"><FontAwesomeIcon icon={faUser} /></span>
+                                    <input className="form-control" name="name" onChange={this.onChange} />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label>Surname</label>
+                                <div className="input-group-append">
+                                    <span className="input-group-text"><FontAwesomeIcon icon={faUser} /></span>
+                                    <input className="form-control" name="surname" onChange={this.onChange} />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label>Email</label>
+                                <div className="input-group-append">
+                                    <span className="input-group-text"><FontAwesomeIcon icon={faEnvelopeSquare} /></span>
+                                    <input className="form-control" name="email" onChange={this.onChange} />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label>Password</label>
+                                <div className="input-group-append">
+                                    <span className="input-group-text"><FontAwesomeIcon icon={faLock} /></span>
+                                    <input className="form-control" name="password" type="password" onChange={this.onChange} />
+                                </div>
+                            </div>
+                            <div className="form-group">
+                                <label>Re-enter password</label>
+                                <div className="input-group-append">
+                                    <span className="input-group-text"><FontAwesomeIcon icon={faLock} /></span>
+                                    <input className="form-control" name="reEnterPassword" type="password" onChange={this.onChange} />
+                                </div>
+                            </div>
+                            <div className="text-center">
+                                <button disabled={pendingApiCall} className="btn btn-primary" onClick={this.onClickSignup}>Add User</button>
+                            </div>
+                            <div className="text-center mt-2">
+                                {pendingApiCall && //conditional rendering deniyor buna pendingApiCall dogruysa devamindaki calisir
+                                    <div className="spinner-border" role="status">
+                                        <span className="sr-only">Loading...</span>
+                                    </div>}
+                            </div>
 
-                </form>
+                        </form>
+                    </div>
+                </div>
             </div>
 
         );
