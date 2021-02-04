e2339f7ca4a357bc7d7574c352b5f3a2e76ae79a
Kutay Yaman <yamankutay1@gmail.com>
2021-02-04 17:08:27 +0300

    Validations are added for userSigunpPage but it has repetitive we will solve this in next commit.

--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -11,7 +11,8 @@ class UserSignupPage extends React.Component {
         email: null,
         password: null,
         reEnterPassword: null,
-        pendingApiCall: false
+        pendingApiCall: false,
+        errors: {}
     }
 
     onChange = (event) => {
@@ -19,8 +20,11 @@ class UserSignupPage extends React.Component {
         //const field = event.target.name;
         //bu yukardaki iki satır yerine Object Destructuring kullanalim
         const { name, value } = event.target; //buna Object Destructuring deniyor. event.target'ın name ini al name degiskenine koy value'sunu al value degiskenine koy
+        const errors = { ...this.state.errors } //... demek kopyasini olustur demek yani errors'un kopyasini olusturduk
+        errors[name] = undefined;
         this.setState({ // render methodunu tekrar cagirir bu olay flutterda vardır.
-            [name]: value
+            [name]: value,
+            errors: errors //yukardaki errors aslinda state'deki errors'un kopyasi alinarak olusturulmustu simdi onu state icindeki errors'a koyduk
         });
     }
 
@@ -40,7 +44,9 @@ class UserSignupPage extends React.Component {
             const response = await signup(body);
         }
         catch (error) {
-
+            if (error.response.data.validationErrors) { //validationErrors undefined degilse
+                this.setState({ errors: error.response.data.validationErrors })
+            }
         }
         finally {
             this.setState({ pendingApiCall: false })
@@ -49,7 +55,8 @@ class UserSignupPage extends React.Component {
     }
 
     render() { // Component'i inherit ettiğimiz için bunu override etmek zorundayız.
-        const { pendingApiCall } = this.state;
+        const { pendingApiCall, errors } = this.state;
+        const { name, surname } = errors;
         return (
             <div className="container">
                 <div className="card mt-2">
@@ -60,14 +67,20 @@ class UserSignupPage extends React.Component {
                                 <label>Your name</label>
                                 <div className="input-group-append">
                                     <span className="input-group-text"><FontAwesomeIcon icon={faUser} /></span>
-                                    <input className="form-control" name="name" onChange={this.onChange} />
+                                    <input className={name ? "form-control is-invalid" : 'form-control'} name="name" onChange={this.onChange} />
+                                    <div className="invalid-feedback ml-1">
+                                        {name}
+                                    </div>
                                 </div>
                             </div>
                             <div className="form-group">
                                 <label>Surname</label>
                                 <div className="input-group-append">
                                     <span className="input-group-text"><FontAwesomeIcon icon={faUser} /></span>
-                                    <input className="form-control" name="surname" onChange={this.onChange} />
+                                    <input className={surname ? "form-control is-invalid" : "form-control"} name="surname" onChange={this.onChange} />
+                                    <div className="invalid-feedback ml-1">
+                                        {surname}
+                                    </div>
                                 </div>
                             </div>
                             <div className="form-group">
