03205cf85a57a81012b1b73340f2433999c4bfd2
Kutay Yaman <yamankutay1@gmail.com>
2021-02-08 02:25:17 +0300

    We deleted the interceptors when the component disappears from the screen. we did this with the "componentWillUnmount" method

--- src/shared/ApiProgress.js
+++ src/shared/ApiProgress.js
@@ -14,12 +14,12 @@ export function withApiProgress(WrappedComponent, apiPath) {
         }
 
         componentDidMount() {//component ilk yuklendigi zaman calisan bir methoddur yani LoginPage componenti yuklenirken
-            axios.interceptors.request.use((request) => {
+            this.requestInterceptor = axios.interceptors.request.use((request) => {
                 this.updateApiCallFor(request.url, true);
                 return request;
             });
 
-            axios.interceptors.response.use((response) => {
+            this.responseInterceptor = axios.interceptors.response.use((response) => {
                 this.updateApiCallFor(response.config.url, false);
 
                 return response;
@@ -27,6 +27,11 @@ export function withApiProgress(WrappedComponent, apiPath) {
                 this.updateApiCallFor(error.config.url, false);
                 throw error;
             })
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.requestInterceptor);
+            axios.interceptors.response.eject(this.responseInterceptor);
         }
 
         updateApiCallFor = (url, inProgress) => {
