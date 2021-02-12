016ee1d9ada303500223832f8c2c5b2e0d58dded
Kutay Yaman <yamankutay1@gmail.com>
2021-02-12 16:39:44 +0300

    When there is a change in the global state, those changes are recorded in local storage and when the application starts again, this information is taken as the initial state before the storage is created

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,16 +1,31 @@
 import { createStore } from 'redux';
 import authReducer from './authReducer';
 
-const loggedInState = {
-    isLoggedIn: true,
-    mail: "sabitemail@gmail.com",
-    name: "kutay",
-    surname: "yaman",
-    password: "12345678"
-};
-
 const configureStore = () => {
-    return createStore(authReducer, loggedInState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); //2. parametremiz baslangic state'imiz oluyor.
+    const manportAuth = localStorage.getItem('manport-auth'); //localstorage'da tuttugumuz oturum bilgilerini cekiyoruz.
+    let stateInLocalStorage = {
+        isLoggedIn: false,
+        mail: undefined,
+        name: undefined,
+        surname: undefined,
+        password: undefined
+    };
+
+    if (manportAuth) { //eger localstorage'da bu bilgiler varsa bunlari STRING'DEN JSON'a ceviriyoruz yoksada zaten stateInLocalStorage login olmamis bir sekilde degerlere sahip olacak
+        try {
+            stateInLocalStorage = JSON.parse(manportAuth) //try bloguna alma amacimiz belki adam local storage'daki degeri eliyle degistirdi ve bu stringe donusturulemeyecek bir hale geldi ozaman hata almayalim diye.
+        } catch (error) {
+
+        }
+    }
+
+    const store = createStore(authReducer, stateInLocalStorage, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); //2. parametremiz baslangic state'imiz oluyor.
+
+    store.subscribe(() => { //store'da her degisim oldugu zaman bu calisacak bir method yaziyoruz.
+        localStorage.setItem('manport-auth', JSON.stringify(store.getState())); //tarayicimizin localStorage'ina bu bilgileri kayit ediyoruz ki sayfa yenilendiginde bu bilgileri kayit etmeyelim yani logout olmamasi icn bu bilgileri bu local storage'dan cekelim
+    })
+
+    return store;
 }
 
 export default configureStore;
