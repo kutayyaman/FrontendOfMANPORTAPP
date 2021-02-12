3713ffa57497d864f88cec3e4a0a803c4cbd404d
Kutay Yaman <yamankutay1@gmail.com>
2021-02-12 17:47:37 +0300

    SECURELS library added to encrypt LocalStorage

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,12 @@
 import { createStore } from 'redux';
 import authReducer from './authReducer';
+import SecureLS from 'secure-ls';
 
-const configureStore = () => {
-    const manportAuth = localStorage.getItem('manport-auth'); //localstorage'da tuttugumuz oturum bilgilerini cekiyoruz.
+const secureLs = new SecureLS();
+
+const getStateFromLocalStorage = () => {
+    //const manportAuth = localStorage.getItem('manport-auth'); //localstorage'da tuttugumuz oturum bilgilerini cekiyoruz.
+    const manportAuth = secureLs.get('manport-auth'); //okunulmaz bir halde kayit etmek icin securels kutuphanesini kullandik
     let stateInLocalStorage = {
         isLoggedIn: false,
         mail: undefined,
@@ -12,17 +16,26 @@ const configureStore = () => {
     };
 
     if (manportAuth) { //eger localstorage'da bu bilgiler varsa bunlari STRING'DEN JSON'a ceviriyoruz yoksada zaten stateInLocalStorage login olmamis bir sekilde degerlere sahip olacak
-        try {
+        /*try {
             stateInLocalStorage = JSON.parse(manportAuth) //try bloguna alma amacimiz belki adam local storage'daki degeri eliyle degistirdi ve bu stringe donusturulemeyecek bir hale geldi ozaman hata almayalim diye.
         } catch (error) {
 
-        }
+        }*/
+        return manportAuth; //yukarsida yorum satirina aldim cunku artik bu json donusum islemlerini securels kutuphanesi kendisi yapiyor
     }
+    return stateInLocalStorage;
+}
 
-    const store = createStore(authReducer, stateInLocalStorage, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); //2. parametremiz baslangic state'imiz oluyor.
+const updateStateInLocalStorage = newState => {
+    //localStorage.setItem('manport-auth', JSON.stringify(newState)); //tarayicimizin localStorage'ina bu bilgileri kayit ediyoruz ki sayfa yenilendiginde bu bilgileri kayit etmeyelim yani logout olmamasi icn bu bilgileri bu local storage'dan cekelim
+    secureLs.set('manport-auth', newState);//okunmaz bir sekilde kayit etmek icin securels kutuphanesini kullandik
+}
+
+const configureStore = () => {
+    const store = createStore(authReducer, getStateFromLocalStorage(), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); //2. parametremiz baslangic state'imiz oluyor.
 
     store.subscribe(() => { //store'da her degisim oldugu zaman bu calisacak bir method yaziyoruz.
-        localStorage.setItem('manport-auth', JSON.stringify(store.getState())); //tarayicimizin localStorage'ina bu bilgileri kayit ediyoruz ki sayfa yenilendiginde bu bilgileri kayit etmeyelim yani logout olmamasi icn bu bilgileri bu local storage'dan cekelim
+        updateStateInLocalStorage(store.getState());
     })
 
     return store;
