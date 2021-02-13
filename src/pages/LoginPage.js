ee8fa7b2436fa8473e01fe778fedd1a7827bef8d
Kutay Yaman <yamankutay1@gmail.com>
2021-02-13 17:42:48 +0300

    LoginPage Component and UserSignupPage Component are changed as a HOOK

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { useEffect, useState } from 'react'
 import Input from '../components/input'
 import { withTranslation } from 'react-i18next';
 import { faEnvelopeSquare, faLock } from '@fortawesome/free-solid-svg-icons';
@@ -8,29 +8,21 @@ import { withApiProgress } from '../shared/ApiProgress';
 import { connect } from 'react-redux';
 import { loginSuccess } from '../redux/authActions';
 
-class LoginPage extends Component {
+const LoginPage = (props) => {
 
-    state = {
-        mail: null,
-        password: null,
-        error: null
-    };
+    const [mail, setMail] = useState();
+    const [password, setpassword] = useState();
+    const [error, setError] = useState();
 
-    onChange = event => {
-        const { name, value } = event.target;
-        this.setState({
-            [name]: value,
-            error: null
-        })
-    }
+    useEffect(() => { //mail veya passwordde bir degisiklik olursa error'u undefined yap dedik.
+        setError(undefined);
+    }, [mail, password]);
 
-    onClickLogin = async event => {
+
+    const onClickLogin = async event => {
         event.preventDefault();
-        const { mail, password, error } = this.state;
-        const { push } = this.props.history; //bu history'i bize Route componenti sagliyor bu Route componentini App.js icerisinde kullanmistik
-        this.setState({
-            error: null
-        })
+        const { push } = props.history; //bu history'i bize Route componenti sagliyor bu Route componentini App.js icerisinde kullanmistik
+        setError(undefined);
         const creds = {
             username: mail, //basic auth username ve password olarak iki parametre alir yani maili username olarak verdim backendde username diye alacagimiz veri aslinda mail olacak
             password: password
@@ -44,38 +36,35 @@ class LoginPage extends Component {
                 surname: surname,
                 password: password
             }
-            this.props.onLoginSuccess(authState);
+            props.onLoginSuccess(authState);
             push('/');
         } catch (errorFromBackend) {
-            this.setState({
-                error: errorFromBackend.response.data.message
-            })
+            setError(errorFromBackend.response.data.message);
         }
 
-    }
+    };
 
-    render() {
-        const { t, pendingApiCall } = this.props;
-        const { error, mail, password } = this.state;
-        const buttonDisabled = !mail || !password;
-        return (
-            <div className="container">
-                <div className="card mt-2">
-                    <div className="card-body">
-                        <h2 className="text-center">{t('Login')}</h2>
-                        <form>
-                            <Input label={t("Email")} name="mail" iconName={faEnvelopeSquare} onChange={this.onChange} />
-                            <Input label={t("Password")} name="password" type="password" iconName={faLock} onChange={this.onChange} />
-                            <ButtonWithProgress onClick={this.onClickLogin} disabled={buttonDisabled || pendingApiCall} pendingApiCall={pendingApiCall} text={t('Login')} loading={t('Loading...')} />
-                            {error && <div className="alert alert-warning mt-1">
-                                {error}
-                            </div>}
-                        </form>
-                    </div>
+
+    const { t, pendingApiCall } = props;
+    const buttonDisabled = !mail || !password;
+    return (
+        <div className="container">
+            <div className="card mt-2">
+                <div className="card-body">
+                    <h2 className="text-center">{t('Login')}</h2>
+                    <form>
+                        <Input label={t("Email")} iconName={faEnvelopeSquare} onChange={(event) => { setMail(event.target.value); }} />
+                        <Input label={t("Password")} type="password" iconName={faLock} onChange={(event) => { setpassword(event.target.value) }} />
+                        <ButtonWithProgress onClick={onClickLogin} disabled={buttonDisabled || pendingApiCall} pendingApiCall={pendingApiCall} text={t('Login')} loading={t('Loading...')} />
+                        {error && <div className="alert alert-warning mt-1">
+                            {error}
+                        </div>}
+                    </form>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
+
 }
 const LoginPageWithTranslation = withTranslation()(LoginPage);
 const LoginPageWithApiProgress = withApiProgress(LoginPageWithTranslation, '/api/auth');
--- src/pages/UserSignupPage.js
+++ src/pages/UserSignupPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { signup } from '../api/apiCalls';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelopeSquare, faLock, faUser } from '@fortawesome/free-solid-svg-icons';
@@ -7,46 +7,41 @@ import { withTranslation } from 'react-i18next';
 import ButtonWithProgress from '../components/ButtonWithProgress'
 import { withApiProgress } from '../shared/ApiProgress';
 
-class UserSignupPage extends React.Component {
+const UserSignupPage = (props) => {
 
-    state = { //bu state değişkeni Component sinifini kalittigimiz icin geliyor
+    const [form, setForm] = useState({
         name: null,
         surname: null,
         email: null,
         password: null,
-        reEnterPassword: null,
-        errors: {},
-        message: null
-    }
+        reEnterPassword: null
+    });
+    const [errors, setErrors] = useState({}); //baslangic degeri olarak obje dedik dikkat!
+    const [message, setMessage] = useState();
 
-    onChange = (event) => {
-        const { t } = this.props;
-        //const name = event.target.value;
-        //const field = event.target.name;
-        //bu yukardaki iki satır yerine Object Destructuring kullanalim
+
+    const onChange = (event) => {
         const { name, value } = event.target; //buna Object Destructuring deniyor. event.target'ın name ini al name degiskenine koy value'sunu al value degiskenine koy
-        const errors = { ...this.state.errors } //... demek kopyasini olustur demek yani errors'un kopyasini olusturduk
-        errors[name] = undefined;
-        if (name === "password" || name === "reEnterPassword") {
-            if (name === "password" && value !== this.state.reEnterPassword) {
-                errors.reEnterPassword = t('Passwords must match')
-            }
-            else if (name === "reEnterPassword" && value !== this.state.password) {
-                errors.reEnterPassword = t('Passwords must match')
-            } else {
-                errors.reEnterPassword = undefined;
-            }
-        }
-        this.setState({ // render methodunu tekrar cagirir bu olay flutterda vardır.
-            [name]: value,
-            errors: errors //yukardaki errors aslinda state'deki errors'un kopyasi alinarak olusturulmustu simdi onu state icindeki errors'a koyduk
-        });
-    }
 
-    onClickSignup = async event => {
+        const errorsCopy = { ...errors } //... demek kopyasini olustur demek yani errors'un kopyasini olusturduk cunku ben setErrors diyince yine errors'u verirsem adresleri ayni oldugu icin degisiklik yok sanacak yapmayacak set etme isini
+        errorsCopy[name] = undefined;
+        setErrors(errorsCopy);
+
+        /*const formCopy = { ...form };
+        formCopy[name] = value;
+        setForm(formCopy);*/ //alttaki sekilde de yapabiliriz bu olayi
+        setForm((previousForm) => { //setErrors'ude istersek boyle yapabiliriz ama ornek olsun diye oyle biraktim onu
+            return {
+                ...previousForm,
+                [name]: value
+            }
+        }); //setForm'a fonksiyonda verebiliriz bu fonksiyon o degiskenin onceki halini parametre olarak alir ve yeni halini geri dondurmeni ister
+    };
+
+    const onClickSignup = async event => {
         event.preventDefault(); //normalde browser formdaki bilgileri alip bize getiriyor bazi isler yapiyor bu methodu cagirarak browser'in bizim icin biseyler yapmasini engelliyoruz
 
-        const { name, surname, email, password } = this.state;
+        const { name, surname, email, password } = form;
         const body = {
             name: name,
             surname: surname,
@@ -56,14 +51,12 @@ class UserSignupPage extends React.Component {
 
         try {
             const response = await signup(body);
-            this.setState({ message: response.data.message })
+            setMessage(response.data.message);
         }
         catch (error) {
             if (error.response.data.validationErrors) { //validationErrors undefined degilse
-                this.setState({
-                    errors: error.response.data.validationErrors,
-                    message: undefined
-                })
+                setErrors(error.response.data.validationErrors);
+                setMessage(undefined);
             }
         }
         finally {
@@ -71,34 +64,38 @@ class UserSignupPage extends React.Component {
 
     }
 
-    render() { // Component'i inherit ettiğimiz için bunu override etmek zorundayız.
-        const { errors, message } = this.state;
-        const { name, surname, email, password, reEnterPassword } = errors;
-        const { t, pendingApiCall } = this.props;
-        return (
-            <div className="container">
-                <div className="card mt-2">
-                    <div className="card-body">
-                        <form>
-                            <h2 className="text-center">{t('Add User')}</h2>
-                            <Input name="name" label={t("Name")} error={name} onChange={this.onChange} iconName={faUser}></Input>
-                            <Input name="surname" label={t("Surname")} error={surname} onChange={this.onChange} iconName={faUser}></Input>
-                            <Input name="email" label={t("Email")} error={email} onChange={this.onChange} iconName={faEnvelopeSquare}></Input>
-                            <Input name="password" label={t("Password")} error={password} onChange={this.onChange} type="password" iconName={faLock}></Input>
-                            <Input name="reEnterPassword" label={t("Re-Enter-Password")} error={reEnterPassword} onChange={this.onChange} type="password" iconName={faLock}></Input>
-                            <ButtonWithProgress onClick={this.onClickSignup} disabled={pendingApiCall || reEnterPassword !== undefined} pendingApiCall={pendingApiCall} text={t('Add User')} loading={t('Loading...')} />
-                            {message && <div class="alert alert-success" role="alert">
-                                {message}
-                            </div>}
-                        </form>
-                    </div>
-                </div>
-            </div>
+    const { name: nameError, surname: surnameError, email: emailError, password: passwordError } = errors;
+    //yukarda biz name, surname gibi degiskenlere zaten bu fonksiyon icerisinde sahip oldugumuz icin error icerisindeki name degiskenini nameError isimli bir degiskene ata demis olduk
+    const { t, pendingApiCall } = props;
 
-        );
+    let reEnterPasswordError;
+    if (form.password !== form.reEnterPassword) {
+        reEnterPasswordError = t('Passwords must match');
     }
 
-}
+    return (
+        <div className="container">
+            <div className="card mt-2">
+                <div className="card-body">
+                    <form>
+                        <h2 className="text-center">{t('Add User')}</h2>
+                        <Input name="name" label={t("Name")} error={nameError} onChange={onChange} iconName={faUser}></Input>
+                        <Input name="surname" label={t("Surname")} error={surnameError} onChange={onChange} iconName={faUser}></Input>
+                        <Input name="email" label={t("Email")} error={emailError} onChange={onChange} iconName={faEnvelopeSquare}></Input>
+                        <Input name="password" label={t("Password")} error={passwordError} onChange={onChange} type="password" iconName={faLock}></Input>
+                        <Input name="reEnterPassword" label={t("Re-Enter-Password")} error={reEnterPasswordError} onChange={onChange} type="password" iconName={faLock}></Input>
+                        <ButtonWithProgress onClick={onClickSignup} disabled={pendingApiCall || reEnterPasswordError !== undefined} pendingApiCall={pendingApiCall} text={t('Add User')} loading={t('Loading...')} />
+                        {message && <div class="alert alert-success" role="alert">
+                            {message}
+                        </div>}
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    );
+};
+
 const UserSignupPageWithTranslation = withTranslation()(UserSignupPage);
 const UserSignupPageWithApiProgress = withApiProgress(UserSignupPageWithTranslation, '/api/users');
 export default UserSignupPageWithApiProgress; //Higher Order Component denir buna.
